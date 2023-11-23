0caa24ea065c8b123448b372c77b0532cc2a4ff5
rolladharani <144825716+rolladharani@users.noreply.github.com>
2023-11-23 06:33:54 +0530

    #include<stdio.h> int main() {     char p;     scanf("%c",&p);     if(p =='v')     {         printf("Violet ");     }     else if(p =='I')     {         printf("Indigo ");     }     else if(p =='B')     {         printf("Blue ");     }     else if(p =='G')     {         printf("Green ");     }     else if(p =='Y')     {         printf("Yellow ");     }     else if(p =='O')     {         printf("Orange ");     }     else if(p =='R')     {         printf("Red ");     }     else     {         printf("-1");     } }

---
+++ Print_the_color_name.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main()
+{
+    char p;
+    scanf("%c",&p);
+    if(p =='v')
+    {
+        printf("Violet
+");
+    }
+    else if(p =='I')
+    {
+        printf("Indigo
+");
+    }
+    else if(p =='B')
+    {
+        printf("Blue
+");
+    }
+    else if(p =='G')
+    {
+        printf("Green
+");
+    }
+    else if(p =='Y')
+    {
+        printf("Yellow
+");
+    }
+    else if(p =='O')
+    {
+        printf("Orange
+");
+    }
+    else if(p =='R')
+    {
+        printf("Red
+");
+    }
+    else
+    {
+        printf("-1");
+    }
+}
