6bc733820cf160cc1593d1f25b69258de1a343fd
rolladharani <144825716+rolladharani@users.noreply.github.com>
2023-12-15 11:10:52 +0530

    #include<stdio.h> int main() {     int n,arr[n],i,sum;     scanf("%d",&n);     for(i=0;i<n;i++)     {        scanf("%d",&arr[i]);        if(arr[i]%2==1)        sum=sum+arr[i];     }     printf("%d",sum); }

---
+++ Odd_Sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[n],i,sum;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+       scanf("%d",&arr[i]);
+       if(arr[i]%2==1)
+       sum=sum+arr[i];
+    }
+    printf("%d",sum);
+}
