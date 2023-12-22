66b0d7ff1b6082a629052c35b5c053db2a65447a
rolladharani <144825716+rolladharani@users.noreply.github.com>
2023-12-22 22:07:03 +0530

    #include<stdio.h> int main() {     int a,b,i,lcm,m,gcd;     scanf("%d%d",&a,&b);     for(i=1;i<=b;i++)     {         lcm=i*a;         if(lcm%b==0)         {         m=lcm;         break;         }     }     gcd=(a*b)/lcm;     printf("%d",gcd); }

---
+++ Greatest_Common_Divisor.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,lcm,m,gcd;
+    scanf("%d%d",&a,&b);
+    for(i=1;i<=b;i++)
+    {
+        lcm=i*a;
+        if(lcm%b==0)
+        {
+        m=lcm;
+        break;
+        }
+    }
+    gcd=(a*b)/lcm;
+    printf("%d",gcd);
+}
