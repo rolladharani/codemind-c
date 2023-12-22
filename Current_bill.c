d853106fefb9d92538e35c78ac0b66656f251f1e
rolladharani <144825716+rolladharani@users.noreply.github.com>
2023-12-22 19:09:19 +0530

    #include<stdio.h> int main() {     int units;     float unitcost,bill;     scanf("%d",&units);     if(units<200)     {         unitcost=1.20;     }     else if(units>=200 && units<400)     {         unitcost=1.50;     }     else if(units>=400 && units<600)     {         unitcost=1.80;     }     else     {         unitcost=2.00;     }      bill=units*unitcost;      if(bill>400)      {         bill+=bill*0.15;      }      else      {          bill+=100;      }     printf("%.2f",bill); }

---
+++ Current_bill.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int units;
+    float unitcost,bill;
+    scanf("%d",&units);
+    if(units<200)
+    {
+        unitcost=1.20;
+    }
+    else if(units>=200 && units<400)
+    {
+        unitcost=1.50;
+    }
+    else if(units>=400 && units<600)
+    {
+        unitcost=1.80;
+    }
+    else
+    {
+        unitcost=2.00;
+    }
+     bill=units*unitcost;
+     if(bill>400)
+     {
+        bill+=bill*0.15;
+     }
+     else
+     {
+         bill+=100;
+     }
+    printf("%.2f",bill);
+}
