b6601006765624fd507762f640a8eb1355b6118d
rolladharani <144825716+rolladharani@users.noreply.github.com>
2024-01-03 13:42:47 +0530

    #include<stdio.h> int main() {     int i,j,k,n;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<=(i-1);j++)         {             printf(" ");         }             for(k=n;k>=i;k--)             {                 printf("%d ",i);             }         printf(" ");     } }

---
+++ Basic_Pattern_6.2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k,n;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=(i-1);j++)
+        {
+            printf(" ");
+        }
+            for(k=n;k>=i;k--)
+            {
+                printf("%d ",i);
+            }
+        printf("
+");
+    }
+}
