a26c3d693677673c3318957af019eff2784b7ae3
Dharani R <144825716+rolladharani@users.noreply.github.com>
2024-03-13 21:49:09 +0530

    #include<iostream> using namespace std; int main() {   int a,b;   cin>>a>>b; cout<<a/b<<endl; }

--- Division.c
+++ Division.c
@@ -1,7 +1,8 @@
-#include<stdio.h>
+#include<iostream>
+using namespace std;
 int main()
 {
-    int a,b;
-    scanf("%d%d",&a,&b);
-    printf("%d",a/b);
-}
+  int a,b;
+  cin>>a>>b;
+cout<<a/b<<endl;
+} 
