dc26185b896ebada869d019193ab19792a753a1c
Dharani R <144825716+rolladharani@users.noreply.github.com>
2024-03-21 22:36:16 +0530

    #include<stdio.h> int main() { int x,y,z; scanf("%d%d%d",&x,&y,&z); int area=2*(x+y); int p =z*area; printf("%d",p); }

--- Frames.c
+++ Frames.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int x,y,z;
-    scanf("%d%d%d",&x,&y,&z);
-    int area=2*(x+y);
-    int p=z*area;
-    printf("%d",p);
-    
+int x,y,z;
+scanf("%d%d%d",&x,&y,&z);
+int area=2*(x+y);
+int p =z*area;
+printf("%d",p);
 }
